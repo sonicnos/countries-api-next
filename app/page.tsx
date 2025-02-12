d96cf33bf429b41b8ef4bbd03e668ba3dd2a2c10
sonicnos <107223123+sonicnos@users.noreply.github.com>
2025-02-12 14:57:51 +0100

    adding some style

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,15 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import CardCountry from "@/components/CardCountry";
+
 import Image from "next/image";
 interface Country {
   name: {
@@ -44,60 +51,65 @@ export default function Home() {
       </h1>
       <div className="flex flex-wrap justify-center gap-3 m-10">
         {data.map((country: Country, index) => (
-          <Dialog key={index}>
-            <DialogTrigger>
-              <Card className="h-[400px] w-[300px] overflow-hidden text-start ">
-                <div className="relative h-52">
-                  <Image
-                    src={country.flags.svg || "/placeholder.svg"}
-                    alt={`Flag of ${country.name.common}`}
-                    layout="fill"
-                    objectFit="cover"
-                  />
-                </div>
-                <CardHeader>
-                  <CardTitle className="text-2xl font-semibold">
-                    {country.name.common}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p className="font-bold">
-                    Capital:{" "}
-                    <span className="font-normal">
-                      {country.capital || "n/a"}
-                    </span>
-                  </p>
+          <Card className="h-[450px] w-[300px] overflow-hidden text-start ">
+            <div className="relative h-52">
+              <Image
+                src={country.flags.svg || "/placeholder.svg"}
+                alt={`Flag of ${country.name.common}`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <CardHeader>
+              <CardTitle className="text-2xl font-semibold">
+                {country.name.common}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="font-bold">
+                Capital:{" "}
+                <span className="font-normal">{country.capital || "n/a"}</span>
+              </p>
 
-                  <p className="font-bold">
-                    Population:{" "}
-                    <span className="font-normal">
-                      {country.population || "n/a"}
-                    </span>
-                  </p>
+              <p className="font-bold">
+                Population:{" "}
+                <span className="font-normal">
+                  {country.population || "n/a"}
+                </span>
+              </p>
 
-                  <p className="font-bold">
-                    Area:{" "}
-                    <span className="font-normal">
-                      {`${country.area} km²` || "n/a"}
-                    </span>
-                  </p>
-                </CardContent>
-              </Card>
-              {/* <CardCountry data={country} /> */}
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle className="text-center text-2xl ">
-                  {country.name.common}
-                </DialogTitle>
-                <DialogDescription>
-                  <img src={country.flags.png} alt="" className="rounded-md" />
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+              <p className="font-bold">
+                Area:{" "}
+                <span className="font-normal">
+                  {`${country.area} km²` || "n/a"}
+                </span>
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Dialog key={index}>
+                <DialogTrigger>
+                  <Button>More info</Button>
+                  {/* <CardCountry data={country} /> */}
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center text-2xl ">
+                      {country.name.common}
+                    </DialogTitle>
+                    <DialogDescription>
+                      <img
+                        src={country.flags.png}
+                        alt=""
+                        className="rounded-md"
+                      />
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
